a1463b9925b0b3325af697ffb5f6c42b2b3d9816
sophia <sophiabaldonado@gmail.com>
2016-11-13 00:11:07 -0800

    Cloth has a texture yayy;

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local g = lovr.graphics
-local pointsX = 40
-local pointsY = 40
-local positions = {}
+local width = 40
+local height = 40
+local vertices = {}
 local previousPositions = {}
 local connections = {}
 local triangleIndices = {}
@@ -12,15 +12,17 @@ function lovr.load()
   local originY = lovr.headset.isPresent() and .8 or 0
 
   -- TODO 2d table?
-  for j = 1, pointsY do
-    local fj = j / pointsY
-    for i = 1, pointsX do
-      local fi = i / pointsX
+  for j = 1, height do
+    local v = j / height
+    for i = 1, width do
+      local u = i / width
+      local x = (u - .5) * gridSize
+      local y = (v - .5) * gridSize + originY
+      local z = -2
 
-      table.insert(positions, {
-        (fi - .5) * gridSize,
-        (fj - .5) * gridSize + originY,
-        -2
+      table.insert(vertices, {
+        x, y, z,
+        u, -v
       })
 
       table.insert(previousPositions, { 0, 0, 0 })
@@ -29,18 +31,18 @@ function lovr.load()
       local n = #connections
       local lastConnection = connections[n]
       if i ~= 1 then lastConnection[1] = n - 1 end
-      if j ~= 1 then lastConnection[2] = n - pointsX end
-      if i ~= pointsX then lastConnection[3] = n + 1 end
-      if j ~= pointsY then lastConnection[4] = n + pointsX end
+      if j ~= 1 then lastConnection[2] = n - width end
+      if i ~= width then lastConnection[3] = n + 1 end
+      if j ~= height then lastConnection[4] = n + width end
     end
   end
 
   local function index(x, y)
-    return x + (y - 1) * pointsX
+    return x + (y - 1) * width
   end
 
-  for j = 1, pointsY do
-    for i = 1, pointsX - 1 do
+  for j = 1, height do
+    for i = 1, width - 1 do
       table.insert(triangleIndices, index(i, j))
       table.insert(triangleIndices, index(i + 1, j))
       table.insert(triangleIndices, index(i + 1, j + 1))
@@ -51,11 +53,38 @@ function lovr.load()
     end
   end
 
-  points = lovr.graphics.newBuffer(positions, 'points')
+  shader = g.newShader([[
+    in vec2 texCoord;
+    out vec2 TexCoord;
+
+    void main() {
+      TexCoord = texCoord;
+
+      gl_Position = lovrProjection * lovrTransform * vec4(position, 1.0);
+    }
+  ]], [[
+    uniform sampler2D cloth;
+    in vec2 TexCoord;
+
+    void main() {
+      color = texture(cloth, TexCoord);
+    }
+  ]])
+
+  local format = {
+    { 'position', 'float', 3 },
+    { 'texCoord', 'float', 2 }
+  }
+
+  points = g.newBuffer(format, vertices, 'points')
   points:setDrawMode('points')
+  texture = g.newTexture('water.png')
+  points:setTexture(texture)
+  g.setShader(shader)
 end
 
 function lovr.update(dt)
+
 
 end
 
